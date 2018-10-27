591703631e109a1a8ecc19040684b3ed032718f8
minhajkhan75 <minhajuddinkhan75@gmail.com>
2018-10-27 12:41:54 +0500

    chepi

--- api/bin/api/main.go
+++ api/bin/api/main.go
@@ -5,8 +5,7 @@ import (
 	"net/http"
 	"net/rpc"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/minhajuddinkhan/reviewzone/comments"
+	"github.com/minhajuddinkhan/reviewzone/api"
 )
 
 const (
@@ -43,31 +42,7 @@ func main() {
 	defer cmtService.Close()
 	defer readerService.Close()
 
-	http.HandleFunc("/go", func(w http.ResponseWriter, r *http.Request) {
-		var csvs [][]string
-		err := readerService.Call("Service.ReadCSVFile", "text.csv", &csvs)
-		if err != nil {
-			panic(err)
-		}
-
-		var csvIDs []string
-		dumpService.Call("Service.DumpCSV", csvs, &csvIDs)
-
-		var commentIds []string
-		req := comments.AddCommentRequest{
-			Comments: []string{"Nice"},
-			CsvID:    csvIDs[0],
-		}
-		err = cmtService.Call("Service.OnCsvComment", req, &commentIds)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var comments []comments.Comment
-		cmtService.Call("Service.GetComments", csvIDs[0], &comments)
-		spew.Dump(comments)
-		w.WriteHeader(http.StatusOK)
-	})
+	http.HandleFunc("/go", api.TestRoute(readerService, dumpService, cmtService))
 
 	http.ListenAndServe(":6000", nil)
 
---
+++ api/routes.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"log"
+	"net/http"
+	"net/rpc"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/minhajuddinkhan/reviewzone/comments"
+)
+
+//TestRoute TestRoute
+func TestRoute(readerService, dumpService, cmtService *rpc.Client) func(w http.ResponseWriter, r *http.Request) {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		var csvs [][]string
+		err := readerService.Call("Service.ReadCSVFile", "text.csv", &csvs)
+		if err != nil {
+			panic(err)
+		}
+
+		var csvIDs []string
+		dumpService.Call("Service.DumpCSV", csvs, &csvIDs)
+
+		var commentIds []string
+		req := comments.AddCommentRequest{
+			Comments: []string{"Nice"},
+			CsvID:    csvIDs[0],
+		}
+		err = cmtService.Call("Service.OnCsvComment", req, &commentIds)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var comments []comments.Comment
+		cmtService.Call("Service.GetComments", csvIDs[0], &comments)
+		spew.Dump(comments)
+		w.WriteHeader(http.StatusOK)
+	}
+}
