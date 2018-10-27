d49075f2f66b777f44395be849036b458d3e283c
minhajkhan75 <minhajuddinkhan75@gmail.com>
2018-10-27 13:45:09 +0500

    what

--- api/routes.go
+++ api/routes.go
@@ -1,10 +1,12 @@
 package api
 
 import (
+	"log"
 	"net/http"
 	"net/rpc"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/minhajuddinkhan/reviewzone/comments"
 )
 
 //TestRoute TestRoute
@@ -18,22 +20,22 @@ func TestRoute(readerService, dumpService, cmtService *rpc.Client) func(w http.R
 		}
 
 		spew.Dump(csvs)
-		// var csvIDs []string
-		// dumpService.Call("Service.DumpCSV", csvs, &csvIDs)
+		var csvIDs []string
+		dumpService.Call("Service.DumpCSV", csvs, &csvIDs)
 
-		// var commentIds []string
-		// req := comments.AddCommentRequest{
-		// 	Comments: []string{"Nice"},
-		// 	CsvID:    csvIDs[0],
-		// }
-		// err = cmtService.Call("Service.OnCsvComment", req, &commentIds)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
+		var commentIds []string
+		req := comments.AddCommentRequest{
+			Comments: []string{"Nice"},
+			CsvID:    csvIDs[0],
+		}
+		err = cmtService.Call("Service.OnCsvComment", req, &commentIds)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		// var comments []comments.Comment
-		// cmtService.Call("Service.GetComments", csvIDs[0], &comments)
-		// spew.Dump(comments)
+		var comments []comments.Comment
+		cmtService.Call("Service.GetComments", csvIDs[0], &comments)
+		spew.Dump(comments)
 		w.Write([]byte(csvs[0][0]))
 		w.WriteHeader(http.StatusOK)
 	}
